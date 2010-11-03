52c67cdf841ffe56a637812e2b2be986549a96ad
Bogdan Gusiev <agresso@gmail.com>
2010-11-03 11:23:15 +0200

    Improved install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 DIR=$(dirname $0)
 
-ln -s $DIR ~/.dotbash
-ln -s ~/.dotbash/.bashrc ~/.bashrc
-ln -s ~/.dotbash/.bash_aliases ~/.bash_aliases
+DOTBASH=$HOME/.dotbash
+
+[ -d $DOTBASH ] || ln -s $DIR $DOTBASH
+
+for FILE_NAME in .bashrc .bash_aliases .irbrc .gemrc .gitconfig .gitignore
+do
+    FILE_PATH=$DOTBASH/$FILE_NAME
+    rm -f $HOME/$FILE_NAME
+    ln -s $FILE_PATH $HOME
+done
