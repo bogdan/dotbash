7820b97911fa68a472c8b74a288ca39086492042
Bogdan Gusiev <agresso@gmail.com>
2011-04-26 17:50:59 +0300

    Ubuntu initial setup script

---
+++ require.sh
@@ -0,0 +1,28 @@
+
+apt-get install skype #exclusive
+
+apt-get install vim-gtk xserver-xorg-input-synaptics-dev ttf-inconsolata kupfer workrave tig chromium-browser flashplugin-installer 
+
+
+
+apt-get install postgresql libpq-dev redis-server libpq-dev openjdk-6-jdk etckeeper
+
+vi /etc/postgresql/8.4/main/pg_hba.conf 
+service postgresql restart
+
+
+vi /etc/etckeeper.conf
+etckeeper init
+
+
+
+
+apt-get install ruby rubygems ruby-dev
+
+apt-get install libxslt1-dev libxml2-dev  curl libreadline-dev ctags
+
+
+gem install --no-ri --no-rdoc bundler shelltoad rails rspec-rails  pivotal_shell resque resque_scheduler pg system_timer sunspot_rails ruby-debug bond wirble ruby-debug-ide rake
+
+
+vi /etc/hosts
