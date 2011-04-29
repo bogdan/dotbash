a7176ecd0d3d7229b26f6a6026185b6dc4796e82
Bogdan Gusiev <agresso@gmail.com>
2011-04-29 10:06:03 +0300

    More things in require

--- require.sh
+++ require.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
 
 apt-get install skype #exclusive
 
-apt-get install vim-gnome xserver-xorg-input-synaptics-dev ttf-inconsolata kupfer workrave tig chromium-browser flashplugin-installer htop nmap transmission-gtk
- ttf-mscorefonts-installer
+apt-get install vim-gnome xserver-xorg-input-synaptics-dev ttf-inconsolata \
+    kupfer workrave tig chromium-browser flashplugin-installer htop nmap \
+    transmission-gtk ttf-mscorefonts-installer shutter
 
 
 
@@ -23,7 +25,9 @@ apt-get install ruby rubygems ruby-dev
 apt-get install libxslt1-dev libxml2-dev  curl libreadline-dev ctags libsqlite3-dev
 
 
-gem install --no-ri --no-rdoc bundler shelltoad rails rspec-rails  pivotal_shell resque resque_scheduler pg system_timer sunspot_rails ruby-debug bond wirble ruby-debug-ide rake 
+gem install --no-ri --no-rdoc bundler shelltoad rails rspec-rails  pivotal_shell \
+    resque resque_scheduler pg system_timer sunspot_rails ruby-debug \
+    bond wirble ruby-debug-ide rake git
 
 
 vi /etc/hosts
