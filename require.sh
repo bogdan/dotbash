00f100ab43358dc21a4c8e4c981a44e657b767dd
Bogdan Gusiev <agresso@gmail.com>
2011-10-25 11:20:06 +0300

    Fix require

--- require.sh
+++ require.sh
@@ -2,9 +2,7 @@
 
 
 #Server goodies
-apt-get install vim ttf-inconsolata tig htop nmap zsh curl ctags ack-grep etckeeper git
-vi /etc/etckeeper.conf
-etckeeper init
+apt-get install vim ttf-inconsolata tig htop nmap zsh curl ctags ack-grep git
 
 # Compilers and interpretators
 apt-get install make g++ gcc nodejs
@@ -14,7 +12,7 @@ apt-get install zip unzip rar unrar
 
 apt-get install skype #exclusive
 
-apt-get install xserver-xorg-input-synaptics-dev kupfer workrave chromium-browser \
+apt-get install xserver-xorg-input-synaptics-dev kupfer chromium-browser \
     vlc shutter vim-gnome wine \
     openjdk-6-jdk \
     flashplugin-installer transmission-gtk ttf-mscorefonts-installer
@@ -26,14 +24,15 @@ apt-get install redis-server
 apt-get install mysql-server libmysql-dev
 
 apt-get install postgresql libpq-dev
-vi /etc/postgresql/8.4/main/pg_hba.conf 
+vi /etc/postgresql/9.1/main/pg_hba.conf 
 service postgresql restart
 
+bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
 
 apt-get install ruby rubygems ruby-dev
 
-apt-get install libxslt1-dev libxml2-dev  ibreadline-dev libsqlite3-dev \
+apt-get install libxslt1-dev libxml2-dev  libreadline-dev libsqlite3-dev \
     
 
 apt-get install libmagick9-dev  #rmagic
@@ -46,4 +45,3 @@ gem install --no-ri --no-rdoc \
     gem-ctags mailcatcher jekyll jekyll_ext
 
 
-vi /etc/hosts
