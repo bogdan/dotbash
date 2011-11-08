d9a2fe768f66935d6f451bed1392b78bc887d315
Bogdan Gusiev <agresso@gmail.com>
2011-11-08 13:02:56 +0200

    psql rc

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DOTBASH=$HOME/.dotbash
 
 [ -d $DOTBASH ] || ln -s $DIR $DOTBASH
 
-for FILE_NAME in .bashrc .bash_aliases .irbrc .gemrc .gitconfig .gitignore .vim bin .zshrc .zsh .rubyrc .rdebugrc
+for FILE_NAME in .bashrc .bash_aliases .irbrc .gemrc .gitconfig .gitignore .vim bin .zshrc .zsh .rubyrc .rdebugrc .psqlrc
 do
     FILE_PATH=$DOTBASH/$FILE_NAME
     rm -f $HOME/$FILE_NAME
