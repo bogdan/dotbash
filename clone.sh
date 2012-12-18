01b004eac0cdb0c4db864b062fb2521fd40024f6
Bogdan Gusiev <agresso@gmail.com>
2012-12-18 16:49:25 +0200

    Install scripts

---
+++ clone.sh
@@ -0,0 +1,3 @@
+git clone git@github.com:bogdan/dotbash.git ~/.dotbash
+cd .dotbash
+./install.sh
--- user.sh
+++ user.sh
@@ -1,5 +1,12 @@
-sudo apt-get install zsh
-sudo useradd -d /home/bogdan --groups users,sudo -m -s `type -p zsh` bogdan 
+USERHOME=/home/bogdan
+
+sudo apt-get install zsh curl git
+sudo useradd -d $USERHOME --groups users,sudo -m -s `type -p zsh` bogdan 
+sudo mkdir -p $USERHOME
+sudo mkdir -p $USERHOME/.ssh
+sudo sh -c "curl https://raw.github.com/bogdan/dotbash/master/authorized_keys >> $USERHOME/.ssh/authorized_keys"
+sudo chown -R bogdan:bogdan $USERHOME
+sudo visudo
+
+sudo -u bogdan sh -c "curl -L https://raw.github.com/bogdan/dotbash/master/clone.sh | bash -s stable"
 sudo passwd bogdan
-
-
