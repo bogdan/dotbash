b20f2f6c731f5f7ec9adc837328d755d0ff258fb
Bogdan Gusiev <agresso@gmail.com>
2012-12-25 15:31:17 +0200

    do

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 
-DOTBASH=$HOME/.dotbash
+USERHOME=/home/bogdan
+DOTBASH=$USERHOME/.dotbash
 
 [ -d $DOTBASH ] || ln -s `pwd` $DOTBASH
 
 for FILE_NAME in .bashrc .bash_aliases .irbrc .gemrc .gitconfig .gitignore .vim bin .zshrc .zsh .rubyrc .rdebugrc .psqlrc .pryrc
 do
     FILE_PATH=$DOTBASH/$FILE_NAME
-    rm -f $HOME/$FILE_NAME
-    ln -s $FILE_PATH $HOME
+    rm -f $USERHOME/$FILE_NAME
+    ln -s $FILE_PATH $USERHOME
 done
 
