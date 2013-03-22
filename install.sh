63964126322b8767255173fb86c98916ddb6404a
Bogdan Gusiev <agresso@gmail.com>
2013-03-22 12:15:35 +0200

    Screenrc

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ DOTBASH=$USERHOME/.dotbash
 
 [ -d $DOTBASH ] || ln -s `pwd` $DOTBASH
 
-for FILE_NAME in .bashrc .bash_aliases .irbrc .gemrc .gitconfig .gitignore .vim bin .zshrc .zsh .rubyrc .rdebugrc .psqlrc .pryrc .tigrc
+for FILE_NAME in .bashrc .bash_aliases .irbrc .gemrc \
+  .gitconfig .gitignore .vim bin .zshrc .zsh .rubyrc \
+  .rdebugrc .psqlrc .pryrc .tigrc .screenrc
 do
     FILE_PATH=$DOTBASH/$FILE_NAME
     rm -f $USERHOME/$FILE_NAME
